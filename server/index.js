b4a41c3d04f8ba65c15771f19b08392d85a3c3cf
Ethan Sanders <ethanedu5@gmail.com>
2018-11-15 12:32:20 -0600

    have working examples of query

--- server/index.js
+++ server/index.js
@@ -3,3 +3,22 @@ const { readFileSync } = require('fs');
 
 const typeDefs = readFileSync(`${__dirname}/schema/typeDefs.graphql`, 'utf8');
 const resolvers = require('./schema/resolvers');
+const options = {
+    port: 3001,
+    endpoint:"/graphql",
+    playground:"/graphiql"
+}
+
+const server = new GraphQLServer(
+    {
+        typeDefs,
+        resolvers,
+        context:req =>({
+            ...req.request
+        })
+    }
+)
+
+server.start(options,()=>{
+    console.log("Server is running!")
+})
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,1 +1,17 @@
 const products = require('../model');
+
+const resolvers = {
+    Query:{
+        test(){
+            return "How's it going?"
+        },
+        getAll(){
+            return products
+        },
+        getOne(parent, args, context){
+            return products.find(product=>product.id === +args.id)
+        }
+    }
+}
+
+module.exports = resolvers;
